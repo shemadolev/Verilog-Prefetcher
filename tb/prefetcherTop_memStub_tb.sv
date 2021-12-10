24ad14f7da6fa6f239b58e4eb5de313a18907104
Dolev Shema <shemadolev@gmail.com>
2021-12-10 13:27:16 +0200

    TB: Add write data & read req

--- tb/prefetcherTop_memStub_tb.sv
+++ tb/prefetcherTop_memStub_tb.sv
@@ -316,42 +316,30 @@ initial begin
 
     `TRANSACTION(s_aw_valid,s_aw_ready)
 
+    //Write data
+    s_axi_wdata = 8'd1;
+    s_axi_wlast = 1'b1;
+
+    `TRANSACTION(s_axi_wvalid,s_axi_wready)
+
+    //Write response (B) should be returned, but not caught
+
+    //Read req of BASE_ADDR
+    s_ar_addr = BASE_ADDR;
+    s_ar_len=1'b0;
+    s_ar_id=5;
+
+    `TRANSACTION(s_ar_valid,s_ar_ready)
+    
+    $display("\n~~~~~~~   1. After read req of addr 0x%h",s_ar_addr);
+    `printTop(prefetcherTop_dut);
+    `printCtrl(prefetcherTop_dut.prCtrlPath);
+    `printData(prefetcherTop_dut.prDataPath);
+    
     for(int i=0;i<10;i++) begin
         #clock_period;
     end
 
-    $finish;
-
-    // //Write data
-    // s_axi_wvalid = 1'b1;
-    // s_axi_wdata = 8'd1;
-    // s_axi_wlast = 1'b1;
-    // while(~(posedge_clk & s_axi_wvalid & s_axi_wready)) begin
-    //     #5;
-    // end
-    // #5;
-    // s_axi_wvalid = 1'b0;
-    // //Write response (B) should be returned, but not caught
-
-    // //Read req of BASE_ADDR
-    // s_ar_valid = 1'b1;
-    // s_ar_addr = BASE_ADDR;
-    // s_ar_len=0;
-    // s_ar_id=5;
-
-    // // #5;
-    // while(~(posedge_clk & s_ar_valid & s_ar_ready)) begin
-    //     #5;
-    // end
-    // #5;
-    
-    // s_ar_valid = 1'b0;
-    
-    // $display("\n~~~~~~~   1. After read req of addr 0x%h",s_ar_addr);
-    // `printTop(prefetcherTop_dut);
-    // `printCtrl(prefetcherTop_dut.prCtrlPath);
-    // `printData(prefetcherTop_dut.prDataPath);
-    
     // while(~(posedge_clk & s_r_valid)) begin
     //     #5;
     // end
